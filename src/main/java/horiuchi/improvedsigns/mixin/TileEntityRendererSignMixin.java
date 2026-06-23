5686932c0667a82ad781f22ae4af3339a54e0e01
Horiuchi <realhoriuchi@gmail.com>
2026-06-23 10:52:21 -0400

    Render flag designs on signs

--- src/main/java/horiuchi/improvedsigns/mixin/TileEntityRendererSignMixin.java
+++ src/main/java/horiuchi/improvedsigns/mixin/TileEntityRendererSignMixin.java
@@ -1,5 +1,7 @@
 package horiuchi.improvedsigns.mixin;
 
+import com.mojang.nbt.tags.CompoundTag;
+import com.mojang.nbt.tags.ListTag;
 import horiuchi.improvedsigns.ImprovedSignsBlocks;
 import horiuchi.improvedsigns.TileEntitySignBackVariablesInterface;
 import net.minecraft.client.Minecraft;
@@ -14,6 +16,7 @@ import net.minecraft.client.render.item.model.ItemModelBlock;
 import net.minecraft.client.render.item.model.ItemModelDispatcher;
 import net.minecraft.client.render.renderer.BlendFactor;
 import net.minecraft.client.render.renderer.GLRenderer;
+import net.minecraft.client.render.renderer.Shaders;
 import net.minecraft.client.render.renderer.State;
 import net.minecraft.client.render.tessellator.TessellatorGeneral;
 import net.minecraft.client.render.texture.stitcher.IconCoordinate;
@@ -26,7 +29,9 @@ import net.minecraft.core.block.entity.TileEntitySign;
 import net.minecraft.core.enums.EnumSignPicture;
 import net.minecraft.core.item.*;
 import net.minecraft.core.item.block.ItemBlock;
+import net.minecraft.core.net.command.TextFormatting;
 import net.minecraft.core.util.helper.Color;
+import net.minecraft.core.util.helper.Direction;
 import net.minecraft.core.util.helper.DyeColor;
 import net.minecraft.core.util.helper.MathHelper;
 import net.minecraft.core.world.pos.TilePos;
@@ -64,6 +69,25 @@ public abstract class TileEntityRendererSignMixin extends TileEntityRenderer<Til
 	@Shadow
 	private static void drawTexturedModalRect(double width, double height, boolean blended, @NotNull IconCoordinate coordinate) {}
 
+
+	@Unique
+	private static final int FLAG_WIDTH = 24;
+	@Unique
+	private static final int FLAG_HEIGHT = 16;
+	@Unique
+	private byte[] unpackFlagColors(byte[] packed) {
+		byte[] unpacked = new byte[384];
+
+		for(int i = 0; i < 96; ++i) {
+			unpacked[i * 4 + 0] = (byte)((packed[i] & 3) >> 0);
+			unpacked[i * 4 + 1] = (byte)((packed[i] & 12) >> 2);
+			unpacked[i * 4 + 2] = (byte)((packed[i] & 48) >> 4);
+			unpacked[i * 4 + 3] = (byte)((packed[i] & 192) >> 6);
+		}
+
+		return unpacked;
+	}
+
 	@Inject(
 		method = "doRender(Lnet/minecraft/client/render/tessellator/TessellatorGeneral;Lnet/minecraft/core/block/entity/TileEntitySign;DDDF)V",
 		at = @At(
@@ -87,7 +111,7 @@ public abstract class TileEntityRendererSignMixin extends TileEntityRenderer<Til
 		GLRenderer.pushFrame();
 		GLRenderer.modelM4f().translate((float)x + 0.5F, (float)y, (float)z + 0.5F);
 		int meta = tileEntity.getBlockMeta();
-		float height, angle;
+		float height, angle = 0.0F;
 		StaticEntityModel model;
 		if (signLogic.isFreeStanding) {
 			angle = ((meta & DyeColor.MASK_COLOR) * 360.0F) / 16.0F;
@@ -99,11 +123,11 @@ public abstract class TileEntityRendererSignMixin extends TileEntityRenderer<Til
 				height = 4.0F;
 			}
 		} else {
-			switch (meta & DyeColor.MASK_COLOR) {
-				case 2 -> angle = 180.0F;
-				case 3 -> angle = 0.0F;
-				case 4 -> angle = 90.0F;
-				default -> angle = -90.0F;
+			switch (Direction.fromIdLenient(meta & DyeColor.MASK_COLOR)) {
+				case NORTH -> angle = 180.0F;
+				case SOUTH -> angle = 0.0F;
+				case WEST -> angle = 90.0F;
+				case EAST -> angle = -90.0F;
 			}
 
 			GLRenderer.modelM4f().rotateY(Math.toRadians(-angle));
@@ -192,19 +216,94 @@ public abstract class TileEntityRendererSignMixin extends TileEntityRenderer<Til
 						this.renderMapInstance.renderMap(t, mapData);
 					}
 				}
+				// Render flag
+				else if (itemStackItem instanceof ItemFlag itemFlag && itemFlag.hasFlagBeenDrawnOn(itemStack)) {
+					CompoundTag flagData = itemStack.getData().getCompoundOrDefault("FlagData", null);
+					if (flagData != null) {
+						byte[] colorIndexes = unpackFlagColors(flagData.getByteArray("Colors"));
+						int[] colorData = {-1, -1, -1};
+
+						ListTag list = flagData.getList("Items");
+						for(int i = 0; i < list.tagCount(); ++i) {
+							CompoundTag compound = (CompoundTag)list.tagAt(i);
+							ItemStack stack = ItemStack.readItemStackFromNbt(compound);
+							if (stack != null && stack.getItem().equals(Items.DYE)) {
+								colorData[i] = Colors.allFlagColors[TextFormatting.get(DyeColor.MASK_COLOR - stack.getMetadata()).id].getARGB();
+							}
+						}
+
+						float flagScale = scale * 0.75F;
+
+						GLRenderer.pushFrame();
+						GLRenderer.modelM4f().rotateY(Math.toRadians(180.0F));
+
+						Minecraft.getMinecraft().textureManager.loadTexture("/assets/minecraft/textures/entity/flag_ui.png").bind();
+
+						double flagBgMinX = ((FLAG_WIDTH / 2.0F) * flagScale);
+						double flagBgMinY = ((FLAG_HEIGHT / 2.0F + 4F) * flagScale);
+						double flagBgMaxX = ((FLAG_WIDTH / 2.0F) * flagScale) - FLAG_WIDTH * flagScale;
+						double flagBgMaxY = ((FLAG_HEIGHT / 2.0F + 4F) * flagScale) - FLAG_HEIGHT * flagScale;
+
+						t.startDrawingQuads();
+						t.addVertexWithUV(flagBgMinX, flagBgMaxY, 0.001, 0, 1);
+						t.addVertexWithUV(flagBgMaxX, flagBgMaxY, 0.001, 1, 1);
+						t.addVertexWithUV(flagBgMaxX, flagBgMinY, 0.001, 1, 0);
+						t.addVertexWithUV(flagBgMinX, flagBgMinY, 0.001, 0, 0);
+						t.draw();
+
+						GLRenderer.enableState(State.BLEND);
+						GLRenderer.setBlendFunc(BlendFactor.ONE_MINUS_SRC_ALPHA, BlendFactor.SRC_COLOR);
+						GLRenderer.setShader(Shaders.COLOR);
+						GLRenderer.globalSetLightEnabled(false);
+
+						for (int color = 0; color < 3; color++) {
+							if (colorData[color] == -1) {
+								continue;
+							}
+
+							GLRenderer.setColor1i(colorData[color]);
+
+							t.startDrawingQuads();
+
+							for (int dx = 0; dx < FLAG_WIDTH; dx++) {
+								for (int dy = 0; dy < FLAG_HEIGHT; dy++) {
+									if (colorIndexes[dx + FLAG_WIDTH * dy] - 1 != color) {
+										continue;
+									}
+
+									double minX = (((FLAG_WIDTH - 2) / 2.0F) * flagScale) - dx * flagScale;
+									double minY = ((FLAG_HEIGHT / 2.0F + 3F) * flagScale) - dy * flagScale;
+									double maxX = minX + flagScale;
+									double maxY = minY + flagScale;
+
+									t.addVertex(minX, maxY, 0.0F);
+									t.addVertex(maxX, maxY, 0.0F);
+									t.addVertex(maxX, minY, 0.0F);
+									t.addVertex(minX, minY, 0.0F);
+								}
+							}
+
+							t.draw();
+						}
+
+						GLRenderer.disableState(State.BLEND);
+						GLRenderer.globalSetLightEnabled(true);
+						GLRenderer.popFrame();
+					}
+				}
 				// Render item or block
 				else {
+					GLRenderer.enableState(State.BLEND);
 					if (itemModelDispatch instanceof ItemModelBlock && itemStackItem instanceof ItemBlock<?> itemBlock &&
 						BlockModelDispatcher.getInstance().getDispatch(itemBlock.getBlock()).shouldItemRender3d()) {
 						GLRenderer.modelM4f().scale(0.5F, 0.5F, 0.001F);
 						GLRenderer.modelM4f().translate(0.0F,  0.25F, 0.0F);
-						GLRenderer.enableState(State.BLEND);
 						itemModelDispatch.render(t, null, itemStack, "gui", false, 1, light, partialTick, false);
-						GLRenderer.disableState(State.BLEND);
 					}
 					else {
 						itemModelDispatch.render(t, null, itemStack, "ground", false, 1, light, partialTick, false);
 					}
+					GLRenderer.disableState(State.BLEND);
 				}
 			}
 
